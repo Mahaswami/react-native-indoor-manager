9763e6c3ce2e839a850ee46b35e11c976ea2c64e
manju <manju@mahaswami.com>
2018-06-12 11:20:13 +0530

    wayfinder: fetching polylne coords by sending destination location

--- android/src/main/java/com/mahaswami/reactlibrary/RNIndoorManagerModule.java
+++ android/src/main/java/com/mahaswami/reactlibrary/RNIndoorManagerModule.java
@@ -10,6 +10,8 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
@@ -23,6 +25,12 @@ import com.indooratlas.android.sdk.resources.IATask;
 import com.indooratlas.android.sdk.IARegion;
 import com.indooratlas.android.sdk.resources.IALatLng;
 import android.os.Looper;
+import java.util.Arrays;
+import java.util.List;
+import java.io.InputStream;
+import android.content.res.Resources;
+import com.indooratlas.android.wayfinding.IARoutingLeg;
+import com.indooratlas.android.wayfinding.IAWayfinder;
 
 public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
 
@@ -33,6 +41,9 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
     private IALatLng mTopRight;
     private IALatLng mTopLeft;
     private String floorPlanId;
+    private IAWayfinder mWayfinder;
+    private IARoutingLeg[] mCurrentRoute;
+    private int mFloor;
   public RNIndoorManagerModule(ReactApplicationContext reactContext) {
     super(reactContext);
   }
@@ -52,6 +63,16 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
       public void run() {
         locationManager = IALocationManager.create(getReactApplicationContext());
         mResourceManager = IAResourceManager.create(getReactApplicationContext());
+                String graphJSON = loadGraphJSON();
+                String errorMessage = "Could not find wayfinding_graph.json from raw resources folder. Cannot do wayfinding.";
+                if (graphJSON == null) {
+                    WritableMap params = Arguments.createMap();
+                    params.putString("message", errorMessage);
+                    sendEvent(getReactApplicationContext(), "indoorAtlasError", params);
+                } else {
+                    mWayfinder = IAWayfinder.create(getReactApplicationContext(), graphJSON);
+                }
+
         locationManager.registerRegionListener(new IARegion.Listener() {
 
           @Override
@@ -62,6 +83,7 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
             params.putString("message", "Enter " + (iaRegion.getType() == IARegion.TYPE_VENUE
                                                                                     ? "VENUE "
                                                                                     : "FLOOR_PLAN ") + iaRegion.getName());
+            params.putBoolean("isTypeFloorPlan", (iaRegion.getType() == IARegion.TYPE_FLOOR_PLAN) ? true : false);
             sendEvent(getReactApplicationContext(), "enterRegion", params);
              if (iaRegion.getType() == IARegion.TYPE_FLOOR_PLAN) {
                 final String newId = iaRegion.getId();
@@ -79,6 +101,7 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
             String id = iaRegion.getId();
             WritableMap params = Arguments.createMap();
             params.putString("id", id);
+            params.putBoolean("isTypeFloorPlan", (iaRegion.getType() == IARegion.TYPE_FLOOR_PLAN) ? true : false);
             params.putString("message", "Exit " + (iaRegion.getType() == IARegion.TYPE_VENUE
                                                                         ? "VENUE "
                                                                         : "FLOOR_PLAN ") + iaRegion.getName());
@@ -95,6 +118,11 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
           public void onLocationChanged(IALocation location) {
             WritableMap params = Arguments.createMap();
             try{
+                mFloor = location.getFloorLevel();
+                if (mWayfinder != null) {
+                    mWayfinder.setLocation(location.getLatitude() ,location.getLongitude(), mFloor);
+                }
+                updateRoute();
                 params.putInt("floor", location.getFloorLevel());
                 params.putDouble("lat", location.getLatitude());
                 params.putDouble("lng", location.getLongitude());
@@ -161,5 +189,103 @@ public class RNIndoorManagerModule extends ReactContextBaseJavaModule {
               }
           }
       }, Looper.getMainLooper()); // deliver callbacks using main looper
+  }
+    @ReactMethod
+    public void getPolylineCoords(ReadableMap coordinates) {
+        if (mWayfinder != null) {
+            mWayfinder.setDestination(coordinates.getDouble("latitude"), coordinates.getDouble("longitude"), 3);
+        }
+        updateRoute();
+  }
+  /**
+   * Load "wayfinding_graph.json" from raw resources folder of the app module
+   * @return
+   */
+  private String loadGraphJSON() {
+      try {
+          Resources res = getReactApplicationContext().getResources();
+          int resourceIdentifier = res.getIdentifier("wayfinding_graph_4", "raw", getReactApplicationContext().getPackageName());
+          InputStream in_s = res.openRawResource(resourceIdentifier);
+          byte[] b = new byte[in_s.available()];
+          in_s.read(b);
+          return new String(b);
+      } catch (Exception e) {
+          return null;
+      }
+  }
+
+  private void updateRoute() {
+      if (mWayfinder == null) {
+          return;
+      }
+      mCurrentRoute = mWayfinder.getRoute();
+      if (mCurrentRoute == null || mCurrentRoute.length == 0) {
+          // Wrong credentials or invalid wayfinding graph
+          return;
+      }
+      visualizeRoute(mCurrentRoute);
+  }
+
+  /**
+   * Visualize the IndoorAtlas Wayfinding path on top of the Google Maps.
+   * @param legs Array of IARoutingLeg objects returned from IAWayfinder.getRoute()
+   */
+  private void visualizeRoute(IARoutingLeg[] legs) {
+      // optCurrent will contain the wayfinding path in the current floor and opt will contain the
+      // whole path, including parts in other floors.
+        WritableMap floorPath = Arguments.createMap();
+        WritableArray optCurrent = Arguments.createArray();
+        WritableArray opt = Arguments.createArray();
+        WritableArray optSteps = Arguments.createArray();
+
+      int floorDiff;
+      for (IARoutingLeg leg : legs) {
+
+        WritableMap jsonStart = Arguments.createMap();
+        WritableMap jsonEnd = Arguments.createMap();
+
+        floorDiff = leg.getBegin().getFloor() - leg.getEnd().getFloor();
+        if (leg.getBegin().getFloor() == mFloor && leg.getEnd().getFloor() == mFloor) {
+
+          jsonStart.putDouble("latitude", leg.getBegin().getLatitude());
+          jsonStart.putDouble("longitude", leg.getBegin().getLongitude());
+
+          jsonEnd.putDouble("latitude", leg.getEnd().getLatitude());
+          jsonEnd.putDouble("longitude", leg.getEnd().getLongitude());
+
+          optCurrent.pushMap(jsonStart);
+          optCurrent.pushMap(jsonEnd);
+
+        }else if(floorDiff == 0){
+          jsonStart.putDouble("latitude", leg.getBegin().getLatitude());
+          jsonStart.putDouble("longitude", leg.getBegin().getLongitude());
+
+          jsonEnd.putDouble("latitude", leg.getEnd().getLatitude());
+          jsonEnd.putDouble("longitude", leg.getEnd().getLongitude());
+
+          opt.pushMap(jsonStart);
+          opt.pushMap(jsonEnd);
+        }else if((floorDiff == -1 || floorDiff == 1)){
+          jsonStart.putDouble("latitude", leg.getBegin().getLatitude());
+          jsonStart.putDouble("longitude", leg.getBegin().getLongitude());
+
+          jsonEnd.putDouble("latitude", leg.getEnd().getLatitude());
+          jsonEnd.putDouble("longitude", leg.getEnd().getLongitude());
+
+          optSteps.pushMap(jsonStart);
+          optSteps.pushMap(jsonEnd);
+        }
+      }
+     if (legs.length > 0) {
+          IARoutingLeg leg = legs[legs.length-1];
+          WritableMap jsonEnd = Arguments.createMap();
+          jsonEnd.putDouble("latitude", leg.getEnd().getLatitude());
+          jsonEnd.putDouble("longitude", leg.getEnd().getLongitude());
+          //opt.pushMap(jsonEnd);
+     }
+    floorPath.putArray("currentFloorPath", optCurrent);
+    floorPath.putArray("otherFloorPath", opt);
+    floorPath.putArray("stairsPath", optSteps);
+    sendEvent(getReactApplicationContext(), "getPolylineCoords", floorPath);
   }
 }
